92c6168761bc074d8ea57b54227e785f9761801f
gagankhokhar <gagankhokhar17@gmail.com>
2017-11-24 01:49:27 +0530

    replace files

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { history } from '../helpers';
-import { alertActions } from '../actions';
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
 import { PrivateRoute } from '../_components';
 import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
